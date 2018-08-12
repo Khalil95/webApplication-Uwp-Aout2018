88549512eff48d10bf4d904608cbc5ca99147704
Unknown <noub.bil@hotmail.com>
2018-08-12 22:01:34 +0200

    n

---
+++ BetterDeal UWP/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+
+        public Person() { }
+    }
+}
